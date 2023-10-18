ddd2cc3f00e3da7f4692004d0f490253c744135a
dinooo <145918377+dindooo@users.noreply.github.com>
2023-10-18 08:25:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljanjeGreskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola = false;
+            do
+            {
+                Console.Write("Upisi svoju godinu rodenja");
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Roden si " + godRod + "godine");
+                    kontrola = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    kontrola=false;
+                }
+            } while (kontrola == true);
+            Console.ReadKey();
+        }
+    }
+}
